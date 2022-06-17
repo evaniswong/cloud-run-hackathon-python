dddb33410b678a8bbe5abba363e0bb8ac292b845
evaniswong <evaniswong@gmail.com>
2022-06-17 16:46:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,76 +1,39 @@
 
-    # Copyright 2020 Google Inc. All Rights Reserved.
-    #
-    # Licensed under the Apache License, Version 2.0 (the "License");
-    # you may not use this file except in compliance with the License.
-    # You may obtain a copy of the License at
-    #
-    #     http://www.apache.org/licenses/LICENSE-2.0
-    #
-    # Unless required by applicable law or agreed to in writing, software
-    # distributed under the License is distributed on an "AS IS" BASIS,
-    # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    # See the License for the specific language governing permissions and
-    # limitations under the License.
+# Copyright 2020 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-    import os
-    import logging
-    import random
-    from flask import Flask, request
+import os
+import logging
+import random
+from flask import Flask, request
 
-    logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
-    logger = logging.getLogger(__name__)
+logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
+logger = logging.getLogger(__name__)
 
-    app = Flask(__name__)
-    moves = ['F', 'T', 'L', 'R']
+app = Flask(__name__)
+moves = ['F', 'T', 'L', 'R']
 
-    @app.route("/", methods=['GET'])
-    def index():
-        return "Let the battle begin!"
+@app.route("/", methods=['GET'])
+def index():
+    return "Let the battle begin!"
 
-    @app.route("/", methods=['POST'])
-    def move():
-        t=0
-        r='s'
-        if t is 1:
-            t=t-1
-            r=1
-        d = 0
-        if d==1:
-            r=3
-        gozero = 0
-        r = request.get_data()
-        ourh = 'https://da-cloud-run-hackathon-python-vdqrirch4a-uc.a.run.app'
-        #logger.info(request.json)
-        logger.info(request.json['arena']['state'][ourh])
-        if request.json['arena']['state'][ourh].get('x') is 0 and request.json['arena']['state'][ourh].get('x') is 0:
-            gozero = 1
-        if gozero==0:    
-            if request.json['arena']['state'][ourh].get('x') is not 0:
-                if request.json['arena']['state'][ourh].get('direction') is not 'S':
-                    t=t+1
-                    r= 2
-            elif request.json['arena']['state'][ourh].get('y') is not 0:
-                if request.json['arena']['state'][ourh].get('direction') is not 'W':
-                    t=t+1
-                    r= 2
-            else:
-                t=t+1
-                r= 0
-        if gozero == 1:
-            if request.json['arena']['state'][ourh].get('direction') is not 'N':
-                t=t+1
-                r= 2
-            else:
-                t=t+1
-                d=d+1
-                r= 0
-                
-        return moves[r]
-                
-                
-            
+@app.route("/", methods=['POST'])
+def move():
+    request.get_data()
+    logger.info(request.json)
+    return moves[random.randrange(len(moves))]
 
-    if __name__ == "__main__":
-    app.run(debug=False,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
-    
+if __name__ == "__main__":
+  app.run(debug=False,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
+  
